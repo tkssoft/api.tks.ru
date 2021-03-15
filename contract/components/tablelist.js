0c1b1b034c5c9670281aa858eac231368fecac28
Shulkevich Vladislav <shulkevich@gmail.com>
2021-03-15 17:58:26 +0300

    Компонент TableList. Обработка клавиш

--- contract/components/tablelist.js
+++ contract/components/tablelist.js
@@ -9,26 +9,59 @@ import { debug } from '../../common/debug';
 
 class TableList extends React.Component {
     constructor (props) {
-        super(props);
+        super(props)
+        this.selected = React.createRef()
+        this.list = React.createRef()
         this.state = {
             selected: 0
         }
     }
 
+    focus() {
+        if (this.selected.current) {
+            this.selected.current.focus()
+        } else {
+            debug('focus errror')
+        }
+    }
+
     componentDidMount() {
-        document.addEventListener('keyup', this.handleKeyPress);
+        this.list.current.addEventListener('keyup', this.handleKeyPress);
     }
 
     componentWillUnmount() {
-        document.removeEventListener('keyup', this.handleKeyPress);
+        this.list.current.removeEventListener('keyup', this.handleKeyPress);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.selected !== this.state.selected) {
+            if (this.selected.current !== null) {
+                if (this.props.onListChange) {
+                    this.props.onListChange(this.state.selected)
+                }
+                if (this.list.current !== null) {
+                    let listrect = this.list.current.getBoundingClientRect();
+                    let currect = this.selected.current.getBoundingClientRect();
+                    if (currect.bottom <= listrect.bottom && currect.top >= listrect.top) {
+                        return
+                    }
+                }
+                if (prevState.selected < this.state.selected) {
+                    this.selected.current.scrollIntoView({block: "end", behavior: "instant"})
+                } else {
+                    this.selected.current.scrollIntoView({block: "start", behavior: "instant"})
+                }
+            }
+        }
     }
 
     handleKeyPress = (e) => {
         const { selected } = this.state
         const { data } = this.props
         if (e.keyCode === keys.VK_RETURN) {
-            this.buttonClick(selected);
-            e.preventDefault();
+            return
+            this.buttonClick(selected, data[selected]);
+            e.preventDefault()
         } else if (e.keyCode === keys.VK_DOWN) {
             // Стрелка вниз
             if (selected !== data.length - 1) {
@@ -43,6 +76,13 @@ class TableList extends React.Component {
             e.preventDefault();
         }
     };
+
+    buttonClick(seleted, rec, e) {
+        const { onSelect } = this.props
+        if (onSelect) {
+            onSelect(rec)
+        }
+    }
 
     itemClick(selected, rec, e) {
         this.setState({
@@ -68,7 +108,7 @@ class TableList extends React.Component {
                     </div>
                 ) }
 
-                <div className={this.getcls("Content", "list-group w-100")} role="tablist">
+                <div className={this.getcls("Content", "list-group w-100")} role="tablist" ref={this.list}>
                     {data.map((rec, i) => {
                         const active = i === this.state.selected ? "active" : "";
                         const linkclass = active ? 'text-white' : 'text-link'
@@ -77,6 +117,8 @@ class TableList extends React.Component {
                                 className={this.getcls("ContentItem", "list-group-item list-group-item-action " + active)}
                                 key={i}
                                 onClick={this.itemClick.bind(this, i, rec)}
+                                onDoubleClick={this.buttonClick.bind(this, i, rec)}
+                                ref={active ? this.selected : ""}
                             >
                                 {isFunction(onContentItem) && onContentItem(rec, i)}
                             </div>
