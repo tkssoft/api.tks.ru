5e234fe65193c535690cfa869e0a232280d204fa
shulkevich@gmail.com <shulkevich@gmail.com>
2021-07-01 15:13:03 +0300

    Возможность загрузки md файлов

--- common/md.js
+++ common/md.js
@@ -1,11 +1,21 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import marked from 'marked';
 
 const Markdown = (props) => {
-    const { markdown } = props;
+    const { markdown, src } = props;
+    const [ md, setmd ] = useState(markdown || '');
+
+    useEffect(() => {
+        if (src) {
+            fetch(src).then((resp) => {
+                return resp.text()
+            }).then((data) => { setmd(data) })
+        }
+    })
+
     return (
-        <div className="ccs-md" dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
+        <div className="ccs-md" dangerouslySetInnerHTML={{ __html: marked(md) }} />
     )
 }
 
